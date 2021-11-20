d46139fc48ad205abf452da4a2e7cce1d49379d3
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-11-20 12:32:20 +0600

    iniitial commit

--- server/utils/redisCaching.js
+++ server/utils/redisCaching.js
@@ -2,22 +2,26 @@ const { redisClient } = require("../server")
 
 const redisCaching = (key, asyncMethod, timeInSec) => {
     return new Promise((resolve, reject) => {
-      redisClient.get(key, async (err, data) => {
-        if (err) reject(err.message);
+      try {
+        redisClient.get(key, async (err, data) => {
+          if (err) reject(err.message);
 
-        if (!data) {
-          const response = await asyncMethod;
-          
-          if (timeInSec) {
-            redisClient.setex(key, timeInSec, JSON.stringify(response));
-          } else {
-            redisClient.set(key, JSON.stringify(response));
+          if (!data) {
+            const response = await asyncMethod;
+
+            if (timeInSec) {
+              redisClient.setex(key, timeInSec, JSON.stringify(response));
+            } else {
+              redisClient.set(key, JSON.stringify(response));
+            }
+            return resolve(response);
           }
-          return resolve(response);
-        }
-        
-        resolve(JSON.parse(data));
-      });
+
+          resolve(JSON.parse(data));
+        });
+      } catch (error) {
+        reject(error.message) 
+      }
     });
 }
  
