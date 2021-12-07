fda5413e8d4b043ddb6fe2a880e0b2536ce309e9
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-12-08 02:26:04 +0600

    commit

--- components/NewsLetterForm.js
+++ components/NewsLetterForm.js
@@ -6,8 +6,8 @@ const NewsLetterForm = () => {
     }
     return (
       <div>
-        <form onSubmit={subscribe} className="flex relative">
-          <input type="text" placeholder="Email" className="text-black pr-5 pl-8 " required/>
+        <form onSubmit={subscribe} className="flex flex-col md:flex-row gap-10 md:gap-0 relative">
+          <input type="text" placeholder="Email" className="text-black pr-5 pl-8 h-12" required/>
           <button
             className="h-12 w-52 flex items-center justify-center bg-gray-900 text-white border border-black uppercase font-semibold transition"
                 >
