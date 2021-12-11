b974a9d2f16f65b68ef2dcebfb4853538b849141
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-12-11 20:06:45 +0600

    working on the custom neon page

--- components/CustmNeonBuilder/Controls/visuals/Visuals.js
+++ components/CustmNeonBuilder/Controls/visuals/Visuals.js
@@ -26,7 +26,7 @@ const Visuals = () => {
     <div
       className="col-span-3 lg:col-span-2 row-span-2 lg:row-span-6 flex text-white  relative overflow-hidden p-4"
       style={{
-        background: `linear-gradient(rgba(0, 0, 0,0.95), rgba(0, 0, 0,0.95)), url('/img/neon-banner${
+        background: `linear-gradient(rgba(0, 0, 0,0.9), rgba(0, 0, 0,0.9)), url('/img/neon-banner${
           state.data.text ? "-5" : ""
         }.jpg')`,
       }}
