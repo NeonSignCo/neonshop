2057c145159565ee43f1e6df6e355555e2442901
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-12-16 01:00:37 +0600

    adding empty cart page contents

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,8 +5,8 @@ import InfoSection from "../components/sections/checkout/InfoSection";
 import PaymentSection from "../components/sections/checkout/PaymentSection";
 import CheckoutContext, { INFO_SECTION, PAYMENT_SECTION, useCheckoutContext } from "../context/CheckoutContext";
 import { useGlobalContext } from "../context/GlobalContext";
-
-
+import Nav from '../components/nav/Nav';
+import Footer from '../components/Footer';
 
 const Checkout = () => {
   return <CheckoutContext>
@@ -18,72 +18,81 @@ export default Checkout
 
 const Container = () => {
   const [state, setState] = useCheckoutContext();
-
+  const [globalState] = useGlobalContext();
   const [showMobileSummary, setshowMobileSummary] = useState(false)
 
    return (
-     <div className="flex flex-col lg:flex-row">
-       <div className="flex-1">
-         <div
-           className="py-16 px-20 text-white text-center"
-           style={{
-             background:
-               "linear-gradient(rgba(0, 0, 0,0.87), rgba(0, 0, 0,0.87)), url('/img/neon-banner-5.jpg')",
-           }}
-         >
-           <CustomLink className="text-3xl md:text-6xl" text="NeonShop" />
-           <div className="flex items-center justify-center gap-3 md:gap-5 mt-5">
-             <CustomLink
-               text="cart"
-               href="/cart"
-               className="text-gray-300"
-               onClick={() => window.scrollTo(0, 0)}
-             />
-             <FaChevronRight />
-             <ToggleBtn
-               section={INFO_SECTION}
-               text="information"
-               state={state}
-               setState={setState}
-             />
-             <FaChevronRight />
-             <ToggleBtn
-               section={PAYMENT_SECTION}
-               text="payment"
-               state={state}
-               setState={setState}
-             />
-           </div>
-         </div>
-         <div className="px-5 lg:px-12 pt-10 grid gap-10">
-           <button
-             className="lg:hidden border border-gray-300 py-2 flex items-center justify-center gap-2"
-             onClick={() => setshowMobileSummary((bool) => !bool)}
-           >
-             <span>Cart Summary</span>
-             <FaChevronDown
-               className={`transition ${showMobileSummary ? "rotate-180" : ""}`}
-             />
-           </button>
-           {showMobileSummary && <CartPreview />}
-           {state.activeSection === INFO_SECTION ? (
-             <InfoSection />
-           ) : (
-             state.activeSection === PAYMENT_SECTION && <PaymentSection />
-           )}
+     <div>
+       {globalState.cart?.items?.length > 0 ? (
+         <div className="flex flex-col lg:flex-row">
+           <div className="flex-1">
+             <div
+               className="py-16 px-20 text-white text-center"
+               style={{
+                 background:
+                   "linear-gradient(rgba(0, 0, 0,0.87), rgba(0, 0, 0,0.87)), url('/img/neon-banner-5.jpg')",
+               }}
+             >
+               <CustomLink className="text-3xl md:text-6xl" text="NeonShop" />
+               <div className="flex items-center justify-center gap-3 md:gap-5 mt-5">
+                 <CustomLink
+                   text="cart"
+                   href="/cart"
+                   className="text-gray-300"
+                   onClick={() => window.scrollTo(0, 0)}
+                 />
+                 <FaChevronRight />
+                 <ToggleBtn
+                   section={INFO_SECTION}
+                   text="information"
+                   state={state}
+                   setState={setState}
+                 />
+                 <FaChevronRight />
+                 <ToggleBtn
+                   section={PAYMENT_SECTION}
+                   text="payment"
+                   state={state}
+                   setState={setState}
+                 />
+               </div>
+             </div>
+             <div className="px-5 lg:px-12 pt-10 grid gap-10">
+               <button
+                 className="lg:hidden border border-gray-300 py-2 flex items-center justify-center gap-2"
+                 onClick={() => setshowMobileSummary((bool) => !bool)}
+               >
+                 <span>Cart Summary</span>
+                 <FaChevronDown
+                   className={`transition ${
+                     showMobileSummary ? "rotate-180" : ""
+                   }`}
+                 />
+               </button>
+               {showMobileSummary && <CartPreview />}
+               {state.activeSection === INFO_SECTION ? (
+                 <InfoSection />
+               ) : (
+                 state.activeSection === PAYMENT_SECTION && <PaymentSection />
+               )}
 
-           <div className="flex justify-center md:justify-start items-center p-2 border-t border-gray-300 gap-8 capitalize">
-             <CustomLink href="/privacy-policy" text="privacy policy" />
-             <CustomLink href="/refund-policy" text="refund policy" />
-             <CustomLink href="/terms-conditions" text="terms and conditions" />
+               <div className="flex justify-center md:justify-start items-center p-2 border-t border-gray-300 gap-8 capitalize">
+                 <CustomLink href="/privacy-policy" text="privacy policy" />
+                 <CustomLink href="/refund-policy" text="refund policy" />
+                 <CustomLink
+                   href="/terms-conditions"
+                   text="terms and conditions"
+                 />
+               </div>
+             </div>
+           </div>
+           <div className="hidden lg:block px-4 py-10">
+             <div className="sticky top-0">
+               <CartPreview />
+             </div>
            </div>
          </div>
-       </div>
-       <div className="hidden lg:block px-4 py-10">
-         <div className="sticky top-0">
-           <CartPreview />
-         </div>
-       </div>
+       ) : <CartEmptySection/> }
      </div>
    );
 }
@@ -208,6 +217,33 @@ const CartItem = ({ item }) => {
           {item.quantity * item.size.price}
         </span>
       </div>
+    </div>
+  );
+}
+
+
+const CartEmptySection = () => {
+  return (
+    <div className="">
+      <Nav />
+      <div className="py-20">
+        <h1 className="text-center text-xl  md:text-3xl mb-5">
+          your cart is empty!
+        </h1>{" "}
+        <div className="flex flex-col sm:flex-row gap-3 items-center sm:justify-center">
+          <CustomLink
+            href="/shop"
+            text="shop now"
+            className="h-12 w-52 flex items-center justify-center bg-black text-white uppercase font-semibold transition hover:text-gray-200"
+          />
+          <CustomLink
+            text="custom neon sign"
+            href="/custom-neon-sign"
+            className="h-12 w-52 flex items-center justify-center bg-white text-black border border-black uppercase font-semibold transition hover:bg-black hover:text-white"
+          />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
