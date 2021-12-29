b03175ef9613541512a9c90b6001eb9a36ebfb70
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-12-29 19:00:33 +0600

    some changes

--- pages/api/stripe/webhook-checkout.js
+++ pages/api/stripe/webhook-checkout.js
@@ -1,9 +1,10 @@
 import nc from "next-connect";
 import bodyParser from "body-parser";
 import { webhookCheckout } from "../../../server/handlers/stripe";
+import ncConfig from "../../../server/utils/ncConfig";
 
 
-const handler = nc().use(bodyParser.raw({ type: "application/json" }))
+const handler = nc(ncConfig).use(bodyParser.raw({ type: "application/json" }))
   .post(webhookCheckout);
 
 export default handler;
