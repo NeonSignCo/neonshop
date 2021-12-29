c040b60273191a0d8110357e94b65695c7c55276
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-12-29 18:19:23 +0600

    stripe signature issue

--- pages/api/stripe/webhook-checkout.js
+++ pages/api/stripe/webhook-checkout.js
@@ -1,4 +1,4 @@
-import { buffer } from "micro";
+// import { buffer } from "micro";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {apiVersion: null});
@@ -6,18 +6,25 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const handler = async (req, res) => {
     if (req.method === "POST") {
-        let sig;
-    try {
-        const buf = await buffer(req);
-        sig = req.headers["stripe-signature"];
-        const event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
+      try { 
+      async function buffer(readable) {
+        const chunks = [];
+        for await (const chunk of readable) {
+          chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+        }
+        return Buffer.concat(chunks);
+      }
+
+      const buf = await buffer(req);
+      const sig = req.headers["stripe-signature"];
+
+        const event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
         return res.json({ status: "success", received: true, event });
     } catch (error) {
         console.log(error)
         return res.status(500).json({
             status: 'fail', 
             message: error.message, 
-            sig
         })
     }
   } else {
