b16d9d89dda65d682a07517eac23da369418f53c
DeveloperRidoy <freelancerridoy247@gmail.com>
2022-01-02 00:07:41 +0600

    some changes

--- pages/api/stripe/webhook-checkout.js
+++ pages/api/stripe/webhook-checkout.js
@@ -1,5 +1,5 @@
 import Stripe from 'stripe'; 
-import { buffer } from 'micro'
+// import { buffer } from 'micro'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2020-08-27",
@@ -8,13 +8,13 @@ const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    const buf = await buffer(req);
+    // const buf = await buffer(req);
     const sig = req.headers["stripe-signature"];
 
     let event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+      event = stripe.webhooks.constructEvent(req.rawBody, sig, webhookSecret);
     } catch (err) {
       res.status(400).send(`Webhook Errors: ${err.message}`);
       return;
