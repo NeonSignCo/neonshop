05f1e4671c5dba9bea18961ece79bbac344db8ce
DeveloperRidoy <freelancerridoy247@gmail.com>
2022-01-01 23:18:37 +0600

    some changes

--- pages/api/stripe/webhook-checkout.js
+++ pages/api/stripe/webhook-checkout.js
@@ -1,4 +1,6 @@
 import Stripe from 'stripe'; 
+import { buffer } from 'micro'
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2020-08-27",
 });
@@ -26,7 +28,7 @@ const handler = async (req, res) => {
 };
 
 export default handler;
-
+ 
 export const config = {
   api: {
     bodyParser: false,
