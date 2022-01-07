a0cabab02116dc825ac13038845bf9f55c2f0e88
DeveloperRidoy <freelancerridoy247@gmail.com>
2022-01-07 21:20:21 +0600

    initial commit

--- pages/api/stripe/checkout-session.js
+++ pages/api/stripe/checkout-session.js
@@ -4,6 +4,6 @@ import dbConnection from "../../../server/middleware/dbConnection";
 import ncConfig from "../../../server/utils/ncConfig";
 import authenticate from '../../../server/middleware/authenticate';
 
-const handler = nc(ncConfig).use(dbConnection, authenticate).post(createCheckoutSession);
+const handler = nc(ncConfig).use(dbConnection).post(createCheckoutSession);
 
 export default handler;
