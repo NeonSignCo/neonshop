0f7bf62188415237d626bc711f75bec4531b1245
DeveloperRidoy <freelancerridoy247@gmail.com>
2022-01-10 00:59:36 +0600

    added about-us photo

--- components/CustmNeonBuilder/CustomNeonBuilder.js
+++ components/CustmNeonBuilder/CustomNeonBuilder.js
@@ -2,14 +2,10 @@ import NeonBuilderContext from "../../context/NeonBuilderContext";
 import Controls from "./Controls/Controls";
 import Visuals from "./Controls/visuals/Visuals";
 
-
-
 const CustomNeonBuilder = () => {
-  
-
   return (
     <NeonBuilderContext>
-      <div className="bg-black h-[calc(100vh-66px)] lg:h-[calc(100vh-68px)] relative grid lg:grid-cols-3 grid-rows-6">
+      <div className="bg-black h-[calc(100vh-58px)] lg:h-[calc(100vh-62px)] relative grid lg:grid-cols-3 grid-rows-6">
         <Visuals />
         <Controls />
       </div>
