7e3efe9122290a97d8da7a58ac31e65c00e77965
DeveloperRidoy <freelancerridoy247@gmail.com>
2022-01-10 12:17:39 +0600

    some changes

--- components/CustmNeonBuilder/CustomNeonBuilder.js
+++ components/CustmNeonBuilder/CustomNeonBuilder.js
@@ -1,8 +1,15 @@
 import NeonBuilderContext from "../../context/NeonBuilderContext";
 import Controls from "./Controls/Controls";
 import Visuals from "./Controls/visuals/Visuals";
+import { useEffect } from "react";
+import { useGlobalContext } from "../../context/GlobalContext";
 
 const CustomNeonBuilder = () => {
+  const [globalState, setGlobalState] = useGlobalContext();
+  useEffect(() => {
+    if (!globalState.showBanner) return; 
+    setGlobalState(state => ({...state, showBanner: false}))
+  }, [])
   return (
     <NeonBuilderContext>
       <div className="bg-black fixed top-0 h-[calc(100vh-58px)] lg:h-[calc(100vh-62px)] relative grid lg:grid-cols-3 grid-rows-6">
--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import getInitialData from "../utils/getInitialData";
-import { useRouter } from 'next/router';
+
 
 const Context = createContext();
 export const useGlobalContext = () => useContext(Context);
@@ -11,13 +11,11 @@ export const ERROR = "ERROR";
 export const NOT_LOGGED_IN_EVALUATED = "NOT_LOGGED_IN_EVALUATED";
 export const NOT_FOUND_EVALUATED = 'NOT_FOUND_EVALUATED';
 const GlobalContext = ({ children, props }) => {
-  const Router = useRouter();
   const serverRendered = props.serverRendered || false;
-
   const [state, setState] = useState({
     serverRendered,
     showMobileMenu: false,
-    showBanner: Router.route!== '/custom-neon-sign',
+    showBanner: true,
     modal: { show: false, type: "", data: {} },
     auth: { loading: !serverRendered, user: props.user  },
     categoryData: { loading: true, categories: [] },
